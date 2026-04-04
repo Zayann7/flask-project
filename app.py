84a0809b71cabed77aa718f3a8927207ef994431
Zayaann7 <zayanthings@gmail.com>
2026-04-04 19:35:32 +0530

    Added JSON file and API route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello Flask App"
+
+@app.route('/api')
+def api():
+    with open('data.json') as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
